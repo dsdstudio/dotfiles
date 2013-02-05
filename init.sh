d8df96b930859880a49e5458eda8d028f1823922
보롱킴 <dsdgun@gmail.com>
2013-02-05 11:15:17 +0900

    mobile-shell(mosh) 추가

--- init.sh
+++ init.sh
@@ -8,7 +8,7 @@ NVM_DIR=~/.nvm
 ## HomeBrew Base configurations 
 ruby -e "$(curl -fsSkL raw.github.com/mxcl/homebrew/go)"
 
-brew update && brew install git tmux mysql tree nginx bash bash-completion mercurial phantomjs
+brew update && brew install git tmux mysql tree nginx bash bash-completion mercurial phantomjs mobile-shell
 # vcprompt => vcs info shell 표시용 프로그램 같은 옵션으로 설치불가능 
 brew install --HEAD vcprompt 
 
