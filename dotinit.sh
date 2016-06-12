251ccdcffad46c1d5f877f1d83a8015df39f34c7
보롱킴 <dsdgun@gmail.com>
2016-06-13 00:21:05 +0900

    설정 링크처리부분 코드 리팩토링

--- dotinit.sh
+++ dotinit.sh
@@ -5,14 +5,15 @@
 # @since 2012.12.25              #
 ##################################
 
-# TODO 1. file check routine 
-# TODO 2. 이미 파일이 있는경우 처리 루틴 
-
 BASE_DIR=$HOME/Dropbox/dotfiles
+LINK_TARGET_FILES=".bash_profile .gitconfig .shells .vim .vimrc .tmux.conf .emacs.d/init.el"
 
-ln -sf $BASE_DIR/.bash_profile $HOME/.bash_profile
-ln -sf $BASE_DIR/.gitconfig $HOME/.gitconfig
-ln -sf $BASE_DIR/.shells $HOME/.shells
-ln -sf $BASE_DIR/.vim $HOME/.vim
-ln -sf $BASE_DIR/.vimrc $HOME/.vimrc
-ln -sf $BASE_DIR/.tmux.conf $HOME/.tmux.conf
+
+# emacs 설정 디렉토리 없는경우 생성
+[ ! -d $HOME/.emacs.d ] && mkdir -p $HOME/.emacs.d
+
+# 미리 정의해놓은 설정파일들 링크 처리
+for FILE_ITEM in $LINK_TARGET_FILES
+do
+	[ ! -h $HOME/$FILE_ITEM ] && ln -sf $BASE_DIR/$FILE_ITEM $HOME/$FILE_ITEM
+done
