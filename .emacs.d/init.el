2b3316cc6d1e03a7ea87ed66c6d5e55d332d817e
보롱킴 <dsdgun@gmail.com>
2016-06-16 23:21:36 +0900

    monokai theme 추가

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,19 +2,32 @@
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
-(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
-(defconst dsdstudio/packages '(clojure-mode
-                               cider
-                               magit
-                               auto-complete
-                               markdown-mode))
+(setq dsdstudio/packages '(clojure-mode
+			   monokai-theme
+			   cider
+			   magit
+			   auto-complete
+			   markdown-mode))
 
 (dolist (pkg dsdstudio/packages)
   (when (not (package-installed-p pkg))
     (package-install pkg)))
 
 (add-hook 'after-init-hook
-	  (lambda () (load-theme 'cyberpunk t) (global-linum-mode 1)))
+	  (lambda () (load-theme 'monokai t) (global-linum-mode 1)))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("561ba4316ba42fe75bc07a907647caa55fc883749ee4f8f280a29516525fc9e8" default))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
