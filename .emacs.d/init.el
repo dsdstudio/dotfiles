bb309ff8c48944d41a159f2595855026ea767637
보롱킴 <dsdgun@gmail.com>
2016-07-14 00:59:28 +0900

    emacs 초기 구동시 package refresh 수행하도록 변경

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -3,6 +3,7 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
+(package-refresh-contents)
 
 (setq dsdstudio/packages '(clojure-mode
                monokai-theme
