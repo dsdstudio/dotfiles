1106a7ba9bb5fc0a499dbaab2867f8baa38cf554
보롱킴 <dsdgun@gmail.com>
2017-02-28 02:54:17 +0900

    [hotfix] orgmode 설정 추가

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,32 +1,39 @@
 (require 'package)
 
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t) 
 (package-initialize)
 (package-refresh-contents)
 
-(setq dsdstudio/packages '(clojure-mode
-               cider
-               magit
-               doom-themes
-               auto-complete
-               company
-               emmet-mode
-               neotree
-               helm
-               helm-projectile
-               helm-ls-git
-               helm-ag
-               ac-js2
-               js2-mode
-               web-mode
-			   gradle-mode
-               flycheck
-               markdown-mode
-			   minimap))
+(setq dsdstudio/packages
+      '(org
+        org-plus-contrib
+        clojure-mode
+        cider
+        magit
+        doom-themes
+        auto-complete
+        company
+        emmet-mode
+        neotree
+        helm
+        helm-projectile
+        helm-ls-git
+        helm-ag
+        ac-js2
+        js2-mode
+        web-mode
+        gradle-mode
+        flycheck
+        ob-go
+        markdown-mode
+        minimap))
 
 (dolist (pkg dsdstudio/packages)
   (when (not (package-installed-p pkg))
     (package-install pkg)))
+
+(require 'all-the-icons)
 (require 'doom-themes)
 (require 'doom-neotree)
 (require 'doom-nlinum)
@@ -38,8 +45,6 @@
       doom-one-brighter-modeline nil
       doom-one-brighter-comments nil
       )
-
-;;; OPTIONAL
 ;; brighter source buffers
 (add-hook 'find-file-hook 'doom-buffer-mode)
 ;; brighter minibuffer when active
@@ -51,19 +56,28 @@
 (add-hook 'js2-mode-hook 'ac-js2-mode)
 (add-hook 'js-mode-hook 'js2-minor-mode)
 
-; 명령 모드일때 helm 자동완성의 지원을 받을수 있도록
+;; org-babel 설정
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((emacs-lisp . t)
+   (C . t)
+   (cpp . t)
+   (clojure . t)
+   (shell . t)))
+
+;; 명령 모드일때 helm 자동완성의 지원을 받을수 있도록
 (global-set-key (kbd "M-x") 'helm-M-x)
 
-; git repository에서 fuzzy search 되도록 
+;; git repository에서 fuzzy search 되도록 
 (global-set-key (kbd "C-x f") 'helm-projectile)
-; 버퍼 찾기시 helm의 buffer 찾기를 사용하도록
+;; 버퍼 찾기시 helm의 buffer 찾기를 사용하도록
 (global-set-key (kbd "C-x b") 'helm-buffers-list)
 
-; tab size space 4
+;; tab size space 4
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 4)
 
-; web-mode 관련 설정
+;; web-mode 관련 설정
 (require 'web-mode)
 (add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.ht[ml]\\'" . web-mode))
