3912453a73115a04643ae3bc5c07c2f8ff09d074
보롱킴 <dsdgun@gmail.com>
2017-07-04 03:36:40 +0900

    [feature] tmuxinator 기본설정 추가

--- init.sh
+++ init.sh
@@ -5,13 +5,16 @@
 NODE_VERSION=v6.2.2
 NVM_DIR=~/.nvm
 
-brew update && brew install git tmux tree bash bash-completion leiningen
+brew update && brew install git tmux tree bash bash-completion leiningen tmuxinator-completion
 # vcprompt => vcs info shell 표시용 프로그램 같은 옵션으로 설치불가능 
 brew install --HEAD vcprompt 
 
 ## nvm 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.29.0/install.sh | bash
 . ~/.nvm/nvm.sh && nvm install $NODE_VERSION && nvm alias default $NODE_VERSION
+
+## tmuxinator 설치 tmux 세션 관리용
+gem install tmuxinator
 
 # Mac Keybindings for Home, end, pageup pagedown 
 mkdir -p ~/Library/KeyBindings/
