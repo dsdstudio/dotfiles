79cac6aa858f3eadfad3c3e43fff65c1cbebecfc
Bohyung Kim <dsdgun@gmail.com>
2018-03-21 21:36:48 +0900

    quicklook markdown 추가

--- init.sh
+++ init.sh
@@ -6,6 +6,11 @@ NODE_VERSION=v6.2.2
 NVM_DIR=~/.nvm
 
 brew update && brew install git tmux tree bash bash-completion leiningen tmuxinator-completion
+brew cask install qlmarkdown
+
+defaults write com.apple.finder QLEnableTextSelection -bool TRUE; killall Finder
+
+
 # vcprompt => vcs info shell 표시용 프로그램 같은 옵션으로 설치불가능 
 brew install --HEAD vcprompt 
 
