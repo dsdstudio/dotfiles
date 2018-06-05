7973f7ab155c43c4da0a64d84fc99fd288001e4b
Bohyung Kim <dsdgun@gmail.com>
2018-06-05 15:30:53 +0900

    [hotfix] script integration

--- init.sh
+++ init.sh
@@ -2,36 +2,45 @@
 # 초기화 선행조건 
 # Xcode command Line tools
 
-NODE_VERSION=v8.11.2
-NVM_DIR=~/.nvm
+package_install(){
+    brew -v update && brew -v install git tmux tree bash bash-completion leiningen tmuxinator-completion the_silver_searcher rbenv fzf
+    brew cask install qlmarkdown
+    # vcprompt => vcs info shell 표시용 프로그램 같은 옵션으로 설치불가능 
+    brew install --HEAD vcprompt 
 
-brew update && brew install git tmux tree bash bash-completion leiningen tmuxinator-completion the_silver_searcher rbenv fzf
-brew cask install qlmarkdown
+    ## nvm 
+    curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
+    . ~/.nvm/nvm.sh && nvm install --lts && nvm alias default node
+}
 
-defaults write com.apple.finder QLEnableTextSelection -bool TRUE; killall Finder
+ruby_package_install() {
+    ## ruby
+    rbenv install 2.4.2
+    rbenv global 2.4.2
+    rbenv rehash
+    ## tmuxinator 설치 tmux 세션 관리용
+    gem install tmuxinator
+}
 
-# vcprompt => vcs info shell 표시용 프로그램 같은 옵션으로 설치불가능 
-brew install --HEAD vcprompt 
+link_files() {
+    # 미리 정의해놓은 설정파일들 링크 처리
+    for file_item in $LINK_TARGET_FILES
+    do
+	    [ ! -h $HOME/$file_item ] && ln -sf $BASE_DIR/$file_item $HOME/$file_item
+    done
+}
 
-## nvm 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
-. ~/.nvm/nvm.sh && nvm install $NODE_VERSION && nvm alias default $NODE_VERSION
+dialog() {
+    local msg
+    echo "설정하시겠습니까? [y/N]"
+    read msg
+    case "$msg" in
+        [y,Y]) package_install && ruby_package_install && link_target_files;;
+        [n,N]) exit -1;;
+        *) echo "다시 입력해주세요" && dialog;;
+    esac
+}
+BASE_DIR=`pwd`
+LINK_TARGET_FILES=".bash_profile .gitconfig .shells .vim .vimrc .tmux.conf .emacs.d"
 
-## ruby
-rbenv install 2.4.2
-rbenv global 2.4.2
-rbenv rehash
-
-## tmuxinator 설치 tmux 세션 관리용
-gem install tmuxinator
-
-# Mac Keybindings for Home, end, pageup pagedown 
-mkdir -p ~/Library/KeyBindings/
-echo " {
-	\"\UF729\" = \"moveToBeginningOfLine:\";
-	\"$\UF729\" = \"moveToBeginningOfLineAndModifySelection:\";
-	\"\UF72B\" = \"moveToEndOfLine:\";
-	\"$\UF72B\" = \"moveToEndOfLineAndModifySelection:\";
-	\"\UF72C\" = \"pageUp:\";
-	\"\UF72D\" = \"pageDown:\";
-}" > ~/Library/KeyBindings/DefaultKeyBinding.dict
+dialog
