962bf70a580680af39c1b1c2e05549aca1aabd2d
Bohyung Kim <dsdgun@gmail.com>
2020-09-12 22:39:58 +0900

    [feature] add linking script

---
+++ link_files.sh
@@ -0,0 +1,7 @@
+BASE_DIR=/Users/bhkim/dotfiles
+LINK_TARGET_FILES=".bash_profile .gitconfig .shells .vim .vimrc .tmux.conf .emacs.d"
+# 미리 정의해놓은 설정파일들 링크 처리
+for file_item in $LINK_TARGET_FILES
+do
+	[ ! -h $HOME/$file_item ] && ln -sf $BASE_DIR/$file_item $HOME/$file_item
+done
