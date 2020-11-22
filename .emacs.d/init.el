00530386af95ff163a07f5a89607cc63e5897d5b
Bohyung Kim <dsdgun@gmail.com>
2020-11-22 21:07:30 +0900

    merge

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,3 +10,17 @@
 ;; finally byte-compile it
 (byte-compile-file (concat user-emacs-directory "init-main.el"))
 (kill-buffer)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (yaml-mode web-mode treemacs swift-mode powerline org-plus-contrib org-gcal ob-swift ob-kotlin ob-go multiple-cursors multi-term minimap markdown-mode magit-popup magit kotlin-mode helm-projectile helm-ls-git helm-ag gradle-mode ghub fzf flycheck-clojure exec-path-from-shell emmet-mode doom-themes company auto-complete ac-js2))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
